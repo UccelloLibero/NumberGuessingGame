f12136852e2244b1be0b92779f79f42c90d9958a
Maya <maya.husic@gmail.com>
2018-10-09 11:32:19 -0400

    Comments added

--- randomNum.js
+++ randomNum.js
@@ -1,7 +1,23 @@
+/*
+The Random Number Guessing game generates a random number between 1 and 10 and allows the player two attemts to guess the number correctly
+*/
+
+// assume the player guessed a wrong number
 var correctGuess = false;
+
+// generate a random number between 1 and 10
 var randomNumber = Math.floor(Math.random() * 10) + 1;
+
+// ask user to guess the number and stores the input into a new variable
 var guess = prompt('I am thinking of a number between 1 and 10, can you guess the number?');
 
+/*
+Test to see if the player:
+ guessed correctly
+ guessed too low
+ guessed too high
+ guessed worng number
+*/
 if (parseInt(guess) === randomNumber) {
   correctGuess = true;
 } else if (parseInt(guess) < randomNumber) {
@@ -15,6 +31,8 @@ if (parseInt(guess) === randomNumber) {
     correctGuess = true;
   }
 }
+
+// test the final response and output response
 if (correctGuess === true) {
   document.write("<h2>You've guess the number!</h2>");
 } else {
